79d49fc91a03a08ca843baf943265b7a341ceb23
anichu <anis.cse5.bu@gmail.com>
2022-11-25 04:11:08 +0600

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-
 import router from "./Routes/Routes/Routes";
 import "./App.css";
 
@@ -8,6 +7,7 @@ function App() {
 	return (
 		<div className="max-w-[1440px] mx-auto">
 			<RouterProvider router={router}></RouterProvider>
+
 			<Toaster></Toaster>
 		</div>
 	);
