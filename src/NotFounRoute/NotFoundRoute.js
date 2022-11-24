c36cca54a0f9396ee99449a23e92b93cafacaa4b
anichu <anis.cse5.bu@gmail.com>
2022-11-25 04:09:32 +0600

    not found page

---
+++ src/NotFounRoute/NotFoundRoute.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFoundRoute = () => {
+	return (
+		<div className="flex flex-col-reverse items-center mt-10">
+			<h2 className="text-5xl text-red-500 font-semibold">
+				The page is not found 🚀🚀
+			</h2>
+			<h1 className="text-6xl text-red-600">404</h1>
+		</div>
+	);
+};
+
+export default NotFoundRoute;
