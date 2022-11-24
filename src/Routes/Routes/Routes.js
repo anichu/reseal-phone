59dd9efaf6d72191e0a5cf02c08666aac9878e60
anichu <anis.cse5.bu@gmail.com>
2022-11-25 04:12:23 +0600

    add dashboard route

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashBoardLayout from "../../LayOut/DashBoardLayout/DashBoardLayout";
 import Main from "../../LayOut/Main/Main";
+import NotFoundRoute from "../../NotFounRoute/NotFoundRoute";
+import AllBuyers from "../../pages/DashBoard/AllBuyers/AllBuyers";
+import AllSellers from "../../pages/DashBoard/AllSelers/AllSellers";
+import DashBoard from "../../pages/DashBoard/DashBoard/DashBoard";
+import DashBoardChildren from "../../pages/DashBoard/DashBoardChildren/DashBoardChildren";
 import Home from "../../pages/Homes/Home/Home";
 import Login from "../../pages/Login/Login";
 import SignUp from "../../pages/SignUp/SignUp";
@@ -22,6 +28,32 @@ const router = createBrowserRouter([
 				element: <SignUp></SignUp>,
 			},
 		],
+	},
+	{
+		path: "/dashboard",
+		element: <DashBoardLayout></DashBoardLayout>,
+		children: [
+			{
+				path: "/dashboard",
+				element: <DashBoard></DashBoard>,
+			},
+			{
+				path: "/dashboard/children",
+				element: <DashBoardChildren></DashBoardChildren>,
+			},
+			{
+				path: "/dashboard/allbuyers",
+				element: <AllBuyers></AllBuyers>,
+			},
+			{
+				path: "/dashboard/allsellers",
+				element: <AllSellers></AllSellers>,
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <NotFoundRoute></NotFoundRoute>,
 	},
 ]);
 
