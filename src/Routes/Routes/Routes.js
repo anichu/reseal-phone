62a296d10ace08b97d8eb70228e4488b6f1d0031
anichu <anis.cse5.bu@gmail.com>
2022-11-25 14:21:25 +0600

    added dashboard routes to routes

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,13 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import DashBoardLayout from "../../LayOut/DashBoardLayout/DashBoardLayout";
 import Main from "../../LayOut/Main/Main";
 import NotFoundRoute from "../../NotFounRoute/NotFoundRoute";
+import AddProduct from "../../pages/DashBoard/AddProduct/AddProduct";
 import AllBuyers from "../../pages/DashBoard/AllBuyers/AllBuyers";
 import AllSellers from "../../pages/DashBoard/AllSelers/AllSellers";
 import DashBoard from "../../pages/DashBoard/DashBoard/DashBoard";
 import DashBoardChildren from "../../pages/DashBoard/DashBoardChildren/DashBoardChildren";
+import MyOrders from "../../pages/DashBoard/MyOrders/MyOrders";
+import MyProducts from "../../pages/DashBoard/MyProducts/MyProducts";
 import Home from "../../pages/Homes/Home/Home";
 import Login from "../../pages/Login/Login";
 import SignUp from "../../pages/SignUp/SignUp";
+import AdminRoute from "../AdminRoute/AdminRoute";
+import BuyerRoute from "../BuyerRoute/BuyerRoute";
+import PrivateRoute from "../PrivateRoutes/PrivateRoutes";
+import SellerRoute from "../SellerRoute/SellerRoute";
 
 const router = createBrowserRouter([
 	{
@@ -31,7 +38,11 @@ const router = createBrowserRouter([
 	},
 	{
 		path: "/dashboard",
-		element: <DashBoardLayout></DashBoardLayout>,
+		element: (
+			<PrivateRoute>
+				<DashBoardLayout></DashBoardLayout>
+			</PrivateRoute>
+		),
 		children: [
 			{
 				path: "/dashboard",
@@ -43,11 +54,43 @@ const router = createBrowserRouter([
 			},
 			{
 				path: "/dashboard/allbuyers",
-				element: <AllBuyers></AllBuyers>,
+				element: (
+					<AdminRoute>
+						<AllBuyers></AllBuyers>
+					</AdminRoute>
+				),
 			},
 			{
 				path: "/dashboard/allsellers",
-				element: <AllSellers></AllSellers>,
+				element: (
+					<AdminRoute>
+						<AllSellers></AllSellers>
+					</AdminRoute>
+				),
+			},
+			{
+				path: "/dashboard/myproducts",
+				element: (
+					<SellerRoute>
+						<MyProducts></MyProducts>
+					</SellerRoute>
+				),
+			},
+			{
+				path: "/dashboard/myorders",
+				element: (
+					<BuyerRoute>
+						<MyOrders></MyOrders>
+					</BuyerRoute>
+				),
+			},
+			{
+				path: "/dashboard/addproduct",
+				element: (
+					<SellerRoute>
+						<AddProduct></AddProduct>
+					</SellerRoute>
+				),
 			},
 		],
 	},
