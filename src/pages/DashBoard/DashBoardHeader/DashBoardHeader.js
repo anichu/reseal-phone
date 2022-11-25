c4c262b5797f3cdd30028ebaf575da654f605265
anichu <anis.cse5.bu@gmail.com>
2022-11-25 14:17:07 +0600

    added conditional dash header items

--- src/pages/DashBoard/DashBoardHeader/DashBoardHeader.js
+++ src/pages/DashBoard/DashBoardHeader/DashBoardHeader.js
@@ -1,40 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContetxt/AuthProvider";
+import useRole from "../../../hooks/useRole";
 
 const DashBoardHeader = () => {
+	const { user } = useContext(AuthContext);
+	const [role] = useRole(user?.email);
+	console.log(role);
 	return (
-		<div className="w-full">
-			<div className="tabs tabs-boxed w-full flex-wrap justify-center">
-				<NavLink
-					to="/dashboard/allbuyers"
-					className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
-				>
-					All Buyers
-				</NavLink>
-				<NavLink
-					to="/dashboard/allsellers"
-					className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
-				>
-					All Sellers
-				</NavLink>
-				<NavLink
-					to="/dashboard/addproduct"
-					className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
-				>
-					Add A Product
-				</NavLink>
-				<NavLink
-					to="/dashboard/myproducts"
-					className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
-				>
-					My Products
-				</NavLink>
-				<NavLink
-					to="/dashboard/myorders"
-					className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
-				>
-					My Orders
-				</NavLink>
+		<div className="w-full ">
+			<div className="tabs py-3 tabs-boxed w-full flex-wrap justify-center">
+				{role && role === "admin" && (
+					<>
+						<NavLink
+							to="/dashboard/allbuyers"
+							className={({ isActive }) =>
+								isActive ? "tab tab-active" : "tab"
+							}
+						>
+							All Buyers
+						</NavLink>
+						<NavLink
+							to="/dashboard/allsellers"
+							className={({ isActive }) =>
+								isActive ? "tab tab-active" : "tab"
+							}
+						>
+							All Sellers
+						</NavLink>
+					</>
+				)}
+
+				{role && role === "seller" && (
+					<>
+						{" "}
+						<NavLink
+							to="/dashboard/addproduct"
+							className={({ isActive }) =>
+								isActive ? "tab tab-active" : "tab"
+							}
+						>
+							Add A Product
+						</NavLink>
+						<NavLink
+							to="/dashboard/myproducts"
+							className={({ isActive }) =>
+								isActive ? "tab tab-active" : "tab"
+							}
+						>
+							My Products
+						</NavLink>
+					</>
+				)}
+
+				{role && role === "buyer" && (
+					<NavLink
+						to="/dashboard/myorders"
+						className={({ isActive }) => (isActive ? "tab tab-active" : "tab")}
+					>
+						My Orders
+					</NavLink>
+				)}
 			</div>
 		</div>
 	);
