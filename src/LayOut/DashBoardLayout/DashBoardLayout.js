7f410b8637c71dec4d92e2393ae5d7d2c381c235
anichu <anis.cse5.bu@gmail.com>
2022-11-27 22:27:06 +0600

    added footer

--- src/LayOut/DashBoardLayout/DashBoardLayout.js
+++ src/LayOut/DashBoardLayout/DashBoardLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../pages/Shared/Navbar/Navbar";
 import DashBoardHeader from "../../pages/DashBoard/DashBoardHeader/DashBoardHeader";
 import { Outlet } from "react-router-dom";
+import Footer from "../../pages/Footer/Footer";
 
 const DashBoardLayout = () => {
 	return (
@@ -9,6 +10,7 @@ const DashBoardLayout = () => {
 			<Navbar></Navbar>
 			<DashBoardHeader></DashBoardHeader>
 			<Outlet></Outlet>
+			<Footer></Footer>
 		</div>
 	);
 };
